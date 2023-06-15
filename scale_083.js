a66d9b59146e6dc48ddb5444c92a6d57938bac44
KouWakai <35868597+KouWakai@users.noreply.github.com>
2023-06-15 13:19:42 +0900

    Delete scale_083.js

--- scale_083.js
+++
@@ -1,6 +0,0 @@
-javascript:(function() {
-  var htmlWrap = document.evaluate('/html/body/div[@id="htmlWrap"]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-  if (htmlWrap) {
-    htmlWrap.style.transform = 'scale(0.83)';
-  }
-})();
