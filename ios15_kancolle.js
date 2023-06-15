32a4b4c5f88f48b1a450784b3ab4f48e15a368a9
KouWakai <35868597+KouWakai@users.noreply.github.com>
2023-06-15 12:40:54 +0900

    Update ios15_kancolle.js

--- ios15_kancolle.js
+++ ios15_kancolle.js
@@ -1,12 +1,16 @@
 class iOS15_kancolle {
-  open(){
-    if(typeof gadgetInfo === undefined) return alart("艦これのゲームページで実行してください");
-    window.open("http:"+gadgetInfo.URL,'_blank');
+  open() {
+    if (typeof gadgetInfo === undefined) return alert("艦これのゲームページで実行してください");
+    window.open("http:" + gadgetInfo.URL, '_blank');
+
+    // opensupportメソッドを実行
+    this.opensupport(document);
   }
-  opensupport(document){
-    var s=document.createElement('link');
-    s.rel='stylesheet';
-    s.href='https://iOS15-kancolle.github.io/iframe.css';
+
+  opensupport(document) {
+    var s = document.createElement('link');
+    s.rel = 'stylesheet';
+    s.href = 'https://iOS15-kancolle.github.io/iframe.css';
     document.body.appendChild(s);
 
     // XPathで要素を取得
@@ -16,25 +20,25 @@ class iOS15_kancolle {
       htmlWrap.style.transform = 'scale(0.83)';
     }
 
-    s=document.createElement('iframe');
-    s.src='https://zekamashi.net/';
-    s.width='48%';
-    s.height='100%';
+    s = document.createElement('iframe');
+    s.src = 'https://zekamashi.net/';
+    s.width = '48%';
+    s.height = '100%';
     document.body.appendChild(s);
-    s=document.createElement('iframe');
-    s.src='https://wikiwiki.jp/kancolle/';
-    s.width='48%';
-    s.height='100%';
+    s = document.createElement('iframe');
+    s.src = 'https://wikiwiki.jp/kancolle/';
+    s.width = '48%';
+    s.height = '100%';
     document.body.appendChild(s);
-    s=document.createElement('iframe');
-    s.src='http://kancollecalc.jp/air_supremacy.html';
-    s.width='48%';
-    s.height='100%';
+    s = document.createElement('iframe');
+    s.src = 'http://kancollecalc.jp/air_supremacy.html';
+    s.width = '48%';
+    s.height = '100%';
     document.body.appendChild(s);
-    s=document.createElement('iframe');
-    s.src='http://kc2ndexpcalc.web.fc2.com';
-    s.width='48%';
-    s.height='100%';
+    s = document.createElement('iframe');
+    s.src = 'http://kc2ndexpcalc.web.fc2.com';
+    s.width = '48%';
+    s.height = '100%';
     document.body.appendChild(s);
   }
 }
